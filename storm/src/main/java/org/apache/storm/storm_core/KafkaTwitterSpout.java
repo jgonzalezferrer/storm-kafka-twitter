fe1694365f210f2ccdf0d59b3d620c0fd1da7f39
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-07 03:25:38 -0800

    Renamed KafkaSpout class.

--- storm/src/main/java/org/apache/storm/storm_core/KafkaTwitterSpout.java
+++ storm/src/main/java/org/apache/storm/storm_core/KafkaTwitterSpout.java
@@ -1,14 +1,18 @@
 package org.apache.storm.storm_core;
 
+import java.util.UUID;
+
 import storm.kafka.BrokerHosts;
 import storm.kafka.KafkaSpout;
 import storm.kafka.SpoutConfig;
 import storm.kafka.ZkHosts;
+import backtype.storm.spout.SchemeAsMultiScheme;
 
-public class KafkaSpout {
-	BrokerHosts hosts = new ZkHosts(zkConnString);
+public class KafkaTwitterSpout {
+	public KafkaTwitterSpout(String zkConn, String topicName){
+	BrokerHosts hosts = new ZkHosts(zkConn);
 	SpoutConfig spoutConfig = new SpoutConfig(hosts, topicName, "/" + topicName, UUID.randomUUID().toString());
 	spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
 	KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
-
+	}
 }
